d2265e79077bcf84428d4b976693ef3f7f197cab
MiLk <hello@emilienkenler.com>
2016-06-05 14:14:03 +0900

    Add tests for the Lobby actor

--- build.sbt
+++ build.sbt
@@ -5,5 +5,8 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4-SNAPSHOT"
+libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % "2.4-SNAPSHOT"
+libraryDependencies += "org.scalactic" %% "scalactic" % "2.2.6"
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
 
 resolvers += "Akka Snapshot Repository" at "http://repo.akka.io/snapshots/"
---
+++ src/test/scala/com/github/milk/warcardgame/lobby/LobbySpec.scala
@@ -0,0 +1,99 @@
+package com.github.milk.warcardgame.lobby
+
+import akka.actor.{Props, Actor, ActorSystem}
+import akka.testkit.{TestProbe, TestKit}
+import com.github.milk.warcardgame.game.CreateGame
+import org.scalatest.{WordSpecLike, BeforeAndAfterAll, MustMatchers}
+
+
+class LobbySpec extends TestKit(ActorSystem("LobbySpec"))
+  with WordSpecLike
+  with MustMatchers
+  with BeforeAndAfterAll {
+
+  import Lobby._
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A Lobby actor" must {
+    "put a joining client in queue" in {
+      val client = TestProbe()
+      val gameSupervisor = TestProbe()
+      val lobby = system.actorOf(Lobby.props(gameSupervisor.ref))
+      client.send(lobby, Join)
+      client.expectMsg(Joined)
+      gameSupervisor.expectNoMsg()
+    }
+
+    "ignore a client joining more than once" in {
+      val client = TestProbe()
+      val gameSupervisor = TestProbe()
+      val lobby = system.actorOf(Lobby.props(gameSupervisor.ref))
+      client.send(lobby, Join)
+      client.expectMsg(Joined)
+      client.send(lobby, Join)
+      client.send(lobby, Join)
+      client.expectNoMsg()
+      gameSupervisor.expectNoMsg()
+    }
+
+    "create a game if 2 different clients are in the queue" in {
+      val client1 = TestProbe()
+      val client2 = TestProbe()
+      val gameSupervisor = TestProbe()
+      val lobby = system.actorOf(Lobby.props(gameSupervisor.ref))
+      client1.send(lobby, Join)
+      client2.send(lobby, Join)
+      gameSupervisor.expectMsgPF() {
+        case CreateGame(set)
+          if set.size == 2 &&
+            set.contains(client1.ref) &&
+            set.contains(client2.ref) => ()
+      }
+    }
+
+    "a client should be able to leave the queue" in {
+      val client = TestProbe()
+      val gameSupervisor = TestProbe()
+      val lobby = system.actorOf(Lobby.props(gameSupervisor.ref))
+      client.send(lobby, Join)
+      client.expectMsg(Joined)
+      client.send(lobby, Leave)
+      client.expectMsg(Left)
+      gameSupervisor.expectNoMsg()
+    }
+
+    "a client should be told if not in the queue" in {
+      val client = TestProbe()
+      val gameSupervisor = TestProbe()
+      val lobby = system.actorOf(Lobby.props(gameSupervisor.ref))
+      client.send(lobby, Join)
+      client.expectMsg(Joined)
+      client.send(lobby, Leave)
+      client.expectMsg(Left)
+      client.send(lobby, Leave)
+      client.expectMsg(NotInQueue)
+      gameSupervisor.expectNoMsg()
+    }
+
+    "a client should no longer be in the queue if a game has been found" in {
+      val client1 = TestProbe()
+      val client2 = TestProbe()
+      val gameSupervisor = TestProbe()
+      val lobby = system.actorOf(Lobby.props(gameSupervisor.ref))
+      client1.send(lobby, Join)
+      client1.expectMsg(Joined)
+      client2.send(lobby, Join)
+      gameSupervisor.expectMsgPF() {
+        case CreateGame(_) => ()
+      }
+      client1.send(lobby, Leave)
+      client1.expectMsg(NotInQueue)
+      client2.send(lobby, Leave)
+      client2.expectMsg(NotInQueue)
+    }
+  }
+
+}
