da57f58fa7da69221507ddb894231c181768e2bd
MiLk <hello@emilienkenler.com>
2016-06-11 11:56:21 +0900

    Move the confirmation mechanism in its own function

--- src/main/scala/com/github/milk/warcardgame/game/Game.scala
+++ src/main/scala/com/github/milk/warcardgame/game/Game.scala
@@ -31,67 +31,67 @@ class Game(players: Set[ActorRef]) extends Actor with akka.actor.ActorLogging {
 
   var decks: Map[ActorRef, List[String]] = Map.empty[ActorRef, List[String]]
 
+  def waitingForConfirmation(confirmed: Set[ActorRef], state: Set[ActorRef] => Actor.Receive)(block: => Unit): Unit = {
+    if (!confirmed.contains(sender)) {
+      val newConfirmed = confirmed + sender
+      if (players.diff(newConfirmed).isEmpty) {
+        block
+      } else context.become(state(newConfirmed))
+    }
+  }
+
   def receive = waitingForStart(Set.empty[ActorRef])
 
   def waitingForStart(confirmed: Set[ActorRef]): Actor.Receive = {
     case GameStartConfirmation =>
-      if (!confirmed.contains(sender)) {
-        val newConfirmed = confirmed + sender
-        if (players.diff(newConfirmed).isEmpty) {
-
-          val deck = Deck.generatePlayersDecks
-          decks += (players.head -> deck._1)
-          decks += (players.tail.head -> deck._2)
-          players.foreach(player => player ! GameStart)
-          context.become(inProgress(Set.empty[ActorRef]))
-
-        } else context.become(waitingForStart(newConfirmed))
+      waitingForConfirmation(confirmed, waitingForStart) {
+        val deck = Deck.generatePlayersDecks
+        decks += (players.head -> deck._1)
+        decks += (players.tail.head -> deck._2)
+        players.foreach(player => player ! GameStart)
+        context.become(inProgress(Set.empty[ActorRef]))
       }
   }
 
   def inProgress(confirmed: Set[ActorRef]): Actor.Receive = {
     case Draw =>
-      if (!confirmed.contains(sender)) {
-        val newConfirmed = confirmed + sender
-        if (players.diff(newConfirmed).isEmpty) {
+      waitingForConfirmation(confirmed, inProgress) {
 
-          val drawnCards = decks.mapValues(_.head)
-          // Remove one card from each deck
-          decks = decks.mapValues(_.tail)
+        val drawnCards = decks.mapValues(_.head)
+        // Remove one card from each deck
+        decks = decks.mapValues(_.tail)
 
-          drawnCards.foreach {
-            case (player, card) => log.info("{} draws a card {}", player.path.name, card)
-          }
+        drawnCards.foreach {
+          case (player, card) => log.info("{} draws a card {}", player.path.name, card)
+        }
 
-          val drawnCardList = drawnCards.values.toList
-          // TODO handle draw
-          val winnerCard = drawnCardList.maxBy(Deck.score)
-          val winnerPlayer = drawnCards.filter(_._2 == winnerCard).head._1
+        val drawnCardList = drawnCards.values.toList
+        // TODO handle draw
+        val winnerCard = drawnCardList.maxBy(Deck.score)
+        val winnerPlayer = drawnCards.filter(_._2 == winnerCard).head._1
 
-          decks += (winnerPlayer -> (decks(winnerPlayer) ::: util.Random.shuffle(drawnCardList)))
+        decks += (winnerPlayer -> (decks(winnerPlayer) ::: util.Random.shuffle(drawnCardList)))
 
-          // Filter the empty decks
-          decks = decks.filter(_._2.nonEmpty)
+        // Filter the empty decks
+        decks = decks.filter(_._2.nonEmpty)
 
-          // Finish if we have no players left
-          if (decks.isEmpty) {
-            players.foreach(_ ! GameOver)
-            context.stop(self)
-          }
-          // Finish if we have a winner
-          else if (decks.size == 1) {
-            val winner = decks.head._1
-            winner ! Victory
-            players.filter(_ != winner).foreach(_ ! GameOver)
-            context.stop(self)
-          }
-          // Next turn if more than 1 player are left
-          else {
-            decks.keys.foreach(_ ! NextTurn)
-            context.become(inProgress(Set.empty[ActorRef]))
-          }
-
-        } else context.become(inProgress(newConfirmed))
+        // Finish if we have no players left
+        if (decks.isEmpty) {
+          players.foreach(_ ! GameOver)
+          context.stop(self)
+        }
+        // Finish if we have a winner
+        else if (decks.size == 1) {
+          val winner = decks.head._1
+          winner ! Victory
+          players.filter(_ != winner).foreach(_ ! GameOver)
+          context.stop(self)
+        }
+        // Next turn if more than 1 player are left
+        else {
+          decks.keys.foreach(_ ! NextTurn)
+          context.become(inProgress(Set.empty[ActorRef]))
+        }
       }
   }
 }
